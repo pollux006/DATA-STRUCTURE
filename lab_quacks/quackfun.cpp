61884e5dda5352f4e0bee27be8884999669d952f
pollux006 <30332677+pollux006@users.noreply.github.com>
2020-02-25 20:54:00 -0600

    Add files via upload

---
+++ lab_quacks/quackfun.cpp
@@ -0,0 +1,167 @@
+/**
+ * @file quackfun.cpp
+ * This is where you will implement the required functions for the
+ * stacks and queues portion of the lab.
+ */
+
+namespace QuackFun {
+
+/**
+ * Sums items in a stack.
+ *
+ * **Hint**: think recursively!
+ *
+ * @note You may modify the stack as long as you restore it to its original
+ * values.
+ *
+ * @note You may use only two local variables of type T in your function.
+ * Note that this function is templatized on the stack's type, so stacks of
+ * objects overloading the + operator can be summed.
+ *
+ * @note We are using the Standard Template Library (STL) stack in this
+ * problem. Its pop function works a bit differently from the stack we
+ * built. Try searching for "stl stack" to learn how to use it.
+ *
+ * @param s A stack holding values to sum.
+ * @return  The sum of all the elements in the stack, leaving the original
+ *          stack in the same state (unchanged).
+ */
+template <typename T>
+T sum(stack<T>& s)
+{
+    T result;
+    T temp;
+    if (s.size()==1){
+      return s.top();
+    }
+    else {
+      result = s.top();
+      temp = result;
+      s.pop();
+      result = sum(s);
+      result = result +temp;
+      s.push(temp);
+      return result;
+
+    }
+
+
+
+    // Your code here
+    return T(); // typedef  MyCustomType; return value (0 for primitive types). Change this!
+                // Note: T() is the default value for objects, and 0 for
+                // primitive types
+}
+
+/**
+ * Checks whether the given string (stored in a queue) has balanced brackets.
+ * A string will consist of square bracket characters, [, ], and other
+ * characters. This function will return true if and only if the square bracket
+ * characters in the given string are balanced. For this to be true, all
+ * brackets must be matched up correctly, with no extra, hanging, or unmatched
+ * brackets. For example, the string "[hello][]" is balanced, "[[][[]a]]" is
+ * balanced, "[]]" is unbalanced, "][" is unbalanced, and "))))[cs225]" is
+ * balanced.
+ *
+ * For this function, you may only create a single local variable of type
+ * `stack<char>`! No other stack or queue local objects may be declared. Note
+ * that you may still declare and use other local variables of primitive types.
+ *
+ * @param input The queue representation of a string to check for balanced brackets in
+ * @return      Whether the input string had balanced brackets
+ */
+bool isBalanced(queue<char> input)
+{
+    stack<char> s;
+    int j = input.size();
+    for (int i=0;i<j;i++){
+      if(input.front()=='['){
+          s.push('[');
+      }
+      else if (input.front()==']'){
+        if(s.size()<1){
+          return false;
+        }
+        else{
+          s.pop();
+        }
+      }
+      input.push(input.front());
+      input.pop();
+    }
+    // @TODO: Make less optimistic
+    if (s.size()==0)  {return true;}
+    else  {return false;}
+}
+
+/**
+ * Reverses even sized blocks of items in the queue. Blocks start at size
+ * one and increase for each subsequent block.
+ *
+ * **Hint**: You'll want to make a local stack variable.
+ *
+ * @note Any "leftover" numbers should be handled as if their block was
+ * complete.
+ *
+ * @note We are using the Standard Template Library (STL) queue in this
+ * problem. Its pop function works a bit differently from the stack we
+ * built. Try searching for "stl stack" to learn how to use it.
+ *
+ * @param q A queue of items to be scrambled
+ */
+template <typename T>
+void scramble(queue<T>& q)
+{
+    stack<T> s;
+
+    // optional: queue<T> q2;
+    // Your code here
+    int j = q.size();
+    if (j<=1) {return;}
+
+    int t = 1;
+    int m = t;
+    T temp;
+
+    for (int i=0;i<j;i++){
+      
+      
+      if(t%2==1){
+        temp = q.front();
+        q.pop();
+        q.push(temp);
+      }
+      else{
+        temp = q.front();
+        s.push(temp);
+        q.pop();
+
+      }
+    m--;
+    if(m==0){
+        if(t%2==0){
+          
+          for (int k=0;k<t;k++){
+            q.push(s.top());
+            s.pop();
+          }
+        }
+        t++;
+        m=t;
+      }
+
+    }
+
+
+  if(!s.empty()){
+    j = s.size();
+    for (int k=0;k<j;k++){
+            q.push(s.top());
+            s.pop();
+          }
+
+  }
+}
+
+
+}
